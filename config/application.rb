e3ec4f88db70537d5a1263dcfa791287a532192a
Stilian Cvetkov <scvetkov@coursera.org>
2021-01-10 20:06:53 +0200

    fix locale

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ Bundler.require(*Rails.groups)
 module MyFavouriteBooks
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
     config.i18n.fallbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
